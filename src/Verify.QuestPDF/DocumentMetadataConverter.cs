861637e25af7592fa65096fa35dbbe39b19990b9
Simon Cropp <simon.cropp@gmail.com>
2022-07-28 08:30:17 +1000

    Update DocumentMetadataConverter.cs

--- src/Verify.QuestPDF/DocumentMetadataConverter.cs
+++ src/Verify.QuestPDF/DocumentMetadataConverter.cs
@@ -6,17 +6,17 @@ class DocumentMetadataConverter :
     public override void Write(VerifyJsonWriter writer, DocumentMetadata value)
     {
         writer.WriteStartObject();
-        writer.WriteProperty(value, value.Author, "Author");
-        writer.WriteProperty(value, value.Creator, "Creator");
-        writer.WriteProperty(value, value.Keywords, "Keywords");
-        writer.WriteProperty(value, value.Producer, "Producer");
-        writer.WriteProperty(value, value.Title, "Title");
-        writer.WriteProperty(value, value.Subject, "Subject");
-        writer.WriteProperty(value, value.CreationDate, "CreationDate");
-        writer.WriteProperty(value, value.ModifiedDate, "ModifiedDate");
-        writer.WriteProperty(value, value.ImageQuality, "ImageQuality");
-        writer.WriteProperty(value, value.PdfA, "PdfA");
-        writer.WriteProperty(value, value.RasterDpi, "RasterDpi");
+        writer.WriteMember(value, value.Author, "Author");
+        writer.WriteMember(value, value.Creator, "Creator");
+        writer.WriteMember(value, value.Keywords, "Keywords");
+        writer.WriteMember(value, value.Producer, "Producer");
+        writer.WriteMember(value, value.Title, "Title");
+        writer.WriteMember(value, value.Subject, "Subject");
+        writer.WriteMember(value, value.CreationDate, "CreationDate");
+        writer.WriteMember(value, value.ModifiedDate, "ModifiedDate");
+        writer.WriteMember(value, value.ImageQuality, "ImageQuality");
+        writer.WriteMember(value, value.PdfA, "PdfA");
+        writer.WriteMember(value, value.RasterDpi, "RasterDpi");
         writer.WriteEnd();
     }
 }
